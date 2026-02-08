ff9e27a7cd6b8daf316e83590060255fe30b6ac4
Nathan Sharp <nathan.james.sharp@proton.me>
2026-02-08 16:07:25 +0000

    created best UI

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "SkyView";
+export const SITE_DESCRIPTION = "Watch videos from the Atmosphere.";
+
+// The BlueSky handle to fetch videos from.
+// Change this to your own handle to see your uploads!
+export const CONTENT_CREATOR_HANDLE = "bsky.app";
